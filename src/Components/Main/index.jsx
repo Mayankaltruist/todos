0610ab990982d3d63f91b5d0b7a60f5a09deaff1
deepakdatarta97 <deepakdatarta97@gmail.com>
2023-08-07 16:17:48 +0530

    Create Main Component

--- src/Components/Main/index.jsx
+++ src/Components/Main/index.jsx
@@ -2,11 +2,36 @@ import React from "react";
 import TodoContainer from "../TodoContainer";
 import Header from "../Header";
 
-function Main() {
+function Main({
+  currentCategoryId,
+  categoryList,
+  handleAddTodo,
+  handleDeleteTodo,
+  handleToggleTodo,
+  handleDeleteCompleteTodos,
+}) {
+
+  const currentCategoryConfig = categoryList.find(
+    (category) => category.id === currentCategoryId
+  );
+
+  if (!currentCategoryConfig) {
+    // You can decide what to render or show when no category is selected
+    return (
+      <main id="main">
+        <h2>Please Select a Category</h2>
+      </main>
+    );
+  }
+
   return (
     <main id="main">
-      <Header />
-      <TodoContainer />
+      <Header CurrentCategoryLabel={currentCategoryConfig.label} />
+      <TodoContainer currentCategoryConfig={currentCategoryConfig}
+      handleAddTodo={handleAddTodo}
+      handleDeleteTodo={handleDeleteTodo}
+      handleToggleTodo={handleToggleTodo}
+      handleDeleteCompleteTodos={handleDeleteCompleteTodos} />
     </main>
   );
 }
