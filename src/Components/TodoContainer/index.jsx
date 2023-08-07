e171118e8a07daf886732f115348af76ed82e77e
deepakdatarta97 <deepakdatarta97@gmail.com>
2023-08-07 16:47:37 +0530

    Create TodoContainer component

--- src/Components/TodoContainer/index.jsx
+++ src/Components/TodoContainer/index.jsx
@@ -1,14 +1,74 @@
-import React from "react";
+import React, { useState } from "react";
 import AddTodoForm from "../AddTodoForm";
 import TodoList from "../TodoList";
 
-function TodoContainer() {
+function TodoContainer({
+  currentCategoryConfig,
+  handleAddTodo,
+  handleDeleteTodo,
+  handleToggleTodo,
+  handleDeleteCompleteTodos,
+}) {
+  const [mode, setMode] = useState("all");
+
+  // Function to handle mode when tab clicked
+  const handleModeChange = (event) => {
+    const modeId = event.target.getAttribute("data-id");
+    setMode(modeId);
+  };
+
+  
+  let todosToShow;
+  // Show all todos
+  if (mode === "all") {
+    todosToShow = currentCategoryConfig.todos;
+    // Show todo with done status false
+  } else if (mode === "active") {
+    todosToShow = currentCategoryConfig.todos.filter(
+      (todo) => todo.done === false
+    );
+    // Show todo with done status true
+  } else {
+    todosToShow = currentCategoryConfig.todos.filter(
+      (todo) => todo.done === true
+    );
+  }
+
   return (
     <section id="todo-container">
-      <AddTodoForm />
-      <Tab />
-      <TodoList />
-      <button>Clear complete item</button>
+      <AddTodoForm
+        handleAddTodo={handleAddTodo}
+        currentCategoryId={currentCategoryConfig.id}
+      />
+      <Tab
+        tabConfig={[
+          {
+            id: "all",
+            label: "All",
+          },
+          {
+            id: "active",
+            label: "Active",
+          },
+          {
+            id: "complete",
+            label: "Complete",
+          },
+        ]}
+        currentTabId={mode}
+        handleTabClick={handleModeChange}
+      />
+      <TodoList
+        handleDeleteTodo={handleDeleteTodo}
+        handleToggleTodo={handleToggleTodo}
+        currentCategoryId={currentCategoryConfig.id}
+      />
+      <button
+        onClick={handleDeleteCompleteTodos}
+        data-category-id={currentCategoryConfig.id}
+      >
+        Clear complete item
+      </button>
     </section>
   );
 }
