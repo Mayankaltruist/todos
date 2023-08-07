ae802eba7cbb95bcdde3cb5cc0bf79b31626bd49
deepakdatarta97 <deepakdatarta97@gmail.com>
2023-08-07 16:54:24 +0530

    Create Header component

--- src/Components/Header/index.jsx
+++ src/Components/Header/index.jsx
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 
-function Header() {
+function Header({ currentCategoryLabel }) {
+  const [date, setDate] = useState("");
+
+  useEffect(() => {
+    // Function to update the date
+    const updateDate = () => {
+      setDate(new Date());
+    };
+    // Call updateDate once on component mount
+    updateDate();
+    // Setup an interval to update the date every secont
+    const timerId = setInterval(updateDate, 1000);
+    // Clear the interval once component unmount
+    return () => {
+      clearInterval(timerId);
+    };
+  }, []);
+
   return (
     <header id="header">
-      <h2></h2>
-      <p></p>
+      <h2>{currentCategoryLabel}</h2>
+      <p>{date.toString()}</p>
     </header>
   );
 }
