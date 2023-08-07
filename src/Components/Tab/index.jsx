8963f92d890304947574d0649b750df430a8266f
deepakdatarta97 <deepakdatarta97@gmail.com>
2023-08-07 16:57:57 +0530

    Create Tab Component

--- src/Components/Tab/index.jsx
+++ src/Components/Tab/index.jsx
@@ -1,13 +1,21 @@
 import React from "react";
 
-function Tab() {
+function Tab({ handleTabClick, tabConfig, currentTabId }) {
   return (
     <ul>
-        <li>
-            
+      {tabConfig.map((tabConfigItem) => (
+        <li
+          data-id={tabConfigItem.id}
+          onClick={handleTabClick}
+          className={
+            tabConfigItem.id === currentTabId ? "tab tab-active" : "tab"
+          }
+        >
+          {tabConfigItem.label}
         </li>
+      ))}
     </ul>
-  )
+  );
 }
 
 export default Tab;
