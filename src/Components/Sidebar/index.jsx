9e4f107224e1993852925c6c075d4878ed8130cd
deepakdatarta97 <deepakdatarta97@gmail.com>
2023-08-07 15:50:15 +0530

    Giving props to sidebar and passing it to child components

--- src/Components/Sidebar/index.jsx
+++ src/Components/Sidebar/index.jsx
@@ -3,12 +3,21 @@ import CategoryList from "../CategoryList";
 import UserInfo from "../UserInfo";
 import AddCategoryForm from "../AddCategoryForm";
 
-function Sidebar() {
+function Sidebar({
+  categoryList,
+  handleAddCategory,
+  handleDeleteCategory,
+  handleSelectCurrentCategory,
+}) {
   return (
     <aside id="sidebar">
       <UserInfo />
-      <CategoryList />
-      <AddCategoryForm />
+      <CategoryList
+        categoryList={categoryList}
+        handleDeleteCategory={handleDeleteCategory}
+        handleSelectCurrentCategory={handleSelectCurrentCategory}
+      />
+      <AddCategoryForm handleAddCategory={handleAddCategory} />
     </aside>
   );
 }
