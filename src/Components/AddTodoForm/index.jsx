c34fa7d095372eee9355c7f53415a63b1d732381
deepakdatarta97 <deepakdatarta97@gmail.com>
2023-08-07 16:32:57 +0530

    Create form component to add todo to todo list

--- src/Components/AddTodoForm/index.jsx
+++ src/Components/AddTodoForm/index.jsx
@@ -1,10 +1,30 @@
 import React from "react";
 
-function AddTodoForm() {
+function AddTodoForm({ currentCategoryId, handleAddTodo }) {
+  const [itemText, setItemText] = useState("");
+
+  function handleFormSubmit(event) {
+    // prevent form default behaviour
+    event.preventDefault();
+    handleAddTodo(currentCategoryId, itemText);
+    //    setting input feild to "" after submit
+    setItemText("");
+  }
+  //  making controlled component
+  function handleItemTextChange(event) {
+    setItemText(event.target.value);
+  }
+
   return (
-    <form className="todo-form">
-      <input type="text"></input>
-      <button type="submit">ADD TODO</button>
+    <form id="todo-form" onSubmit={handleFormSubmit}>
+      <input
+        type="text"
+        value={itemText}
+        onChange={handleItemTextChange}
+      ></input>
+      <button type="submit" disable={itemText.length === 0}>
+        ADD TODO
+      </button>
     </form>
   );
 }
