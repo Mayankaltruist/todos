65fa436081867309f69a5c68d7b96841ae519b4a
deepakdatarta97 <deepakdatarta97@gmail.com>
2023-08-07 15:03:17 +0530

    create initial state of the App

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,51 @@
-import React from "react";
+import React, { Fragment } from "react";
+import Sidebar from "./Components/Sidebar";
+import Main from "./Components/Main";
 
 function App() {
-  return;
+  const initialCategoryList = [
+    {
+      id: "workout",
+      label: "Workout",
+      todos: [
+        {
+          id: "1",
+          label: "Running",
+          done: false,
+        },
+        {
+          id: "2",
+          label: "Go to Gym",
+          done: false,
+        },
+      ],
+    },
+    {
+      id: "study",
+      label: "Study",
+      todos: [
+        {
+          id: "1",
+          label: "Read Book",
+          done: false,
+        },
+        {
+          id: "2",
+          label: "Write journel",
+          done: false,
+        },
+      ],
+    },
+  ];
+
+  
+
+  return (
+    <Fragment>
+      <Sidebar />
+      <Main />
+    </Fragment>
+  );
 }
 
 export default App;
