98eb7d0787b3468bf91cefd5e5ab21d3db8c29b3
deepakdatarta97 <deepakdatarta97@gmail.com>
2023-08-07 16:12:18 +0530

    create component to add category in category list

--- src/Components/AddCategoryForm/index.jsx
+++ src/Components/AddCategoryForm/index.jsx
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 
-function AddCategoryForm() {
+function AddCategoryForm({ handleAddCategory }) {
+  // setting state of input
+  const [categoryText, setCategoryText] = useState("");
+
+  // making component a controlled component
+  const handleChange = (event) => {
+    setCategoryText(event.target.value);
+  };
+
+  const handleSubmit = (event) => {
+    // prevent form default beaviour
+    event.preventDefault();
+    handleAddCategory(categoryText);
+    // reset category text to ""
+    setCategoryText("");
+  };
+
   return (
-    <form className="add-category">
-      <input type="text"></input>
+    <form id="form" onSubmit={handleSubmit}>
+      <input type="text" value={categoryText} onChange={handleChange}></input>
       <button type="submit">ADD CATEGORY</button>
     </form>
   );
