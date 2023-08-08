28b9ff2d6664046bf02ff329f1dee6951394a99a
deepakdatarta97 <deepakdatarta97@gmail.com>
2023-08-08 07:54:13 +0530

    Create constants for tab config

---
+++ src/constants/index.js
@@ -0,0 +1,20 @@
+const ALL = "all";
+const ACTIVE = "active";
+const COMPLETE = "complete";
+
+const tabConfig = [
+  {
+    id: ALL,
+    label: "All",
+  },
+  {
+    id: ACTIVE,
+    label: "Active",
+  },
+  {
+    id: COMPLETE,
+    label: "Complete",
+  },
+];
+
+export { tabConfig, ALL, ACTIVE, COMPLETE };
